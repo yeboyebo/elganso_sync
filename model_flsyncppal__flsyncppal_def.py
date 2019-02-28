074e2e33ef89e93dfc1010ef691a29ff52a12185
xavikortes <javiercortesveliz@gmail.com>
2019-02-28 13:37:29 +0100

    Cambio nombre funcion elganso_sync

--- model_flsyncppal__flsyncppal_def.py
+++ model_flsyncppal__flsyncppal_def.py
@@ -2,12 +2,12 @@
 # @class_declaration elganso_sync #
 class elganso_sync(flsyncppal):
 
-    def flsyncppal_get_customer(self):
+    def elganso_sync_get_customer(self):
         return "elganso"
 
     def __init__(self, context=None):
         super().__init__(context)
 
     def get_customer(self):
-        return self.ctx.flsyncppal_get_customer()
+        return self.ctx.elganso_sync_get_customer()
 
