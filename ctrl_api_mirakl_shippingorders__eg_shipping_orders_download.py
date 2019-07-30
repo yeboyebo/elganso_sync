e7c11db30e19a75b021cb61c0acfc83b45d66c80
Lorena Lopez Lopez <lorena@yeboyebo.es>
2019-07-30 10:10:06 +0200

    Correccion en mirakl shipping

--- ctrl_api_mirakl_shippingorders__eg_shipping_orders_download.py
+++ ctrl_api_mirakl_shippingorders__eg_shipping_orders_download.py
@@ -5,13 +5,13 @@ from controllers.base.mirakl.orders.controllers.shipping_orders_download import 
 
 class EgMiraklShippingOrdersDownload(ShippingOrdersDownload):
 
-    orders_url = "https://marketplace.elcorteingles.es/api/orders?start_update_date={}"
-    orders_test_url = "https://marketplace.elcorteingles.es/api/orders?start_update_date={}"
+    shipping_url = "https://marketplace.elcorteingles.es/api/orders?order_state_codes=SHIPPING&order_ids={}"
+    shipping_test_url = "https://marketplace.elcorteingles.es/api/orders?order_state_codes=SHIPPING&order_ids={}"
 
     # orders_url = "https://marketplace.elcorteingles.es/api/orders?order_state_codes=SHIPPING&start_update_date={}"
     # orders_test_url = "https://marketplace.elcorteingles.es/api/orders?order_state_codes=SHIPPING&start_update_date={}"
 
-    def __init__(self, process, params=None):
+    def __init__(self, params=None):
         super().__init__("egmiraklshippingorders", params)
 
         self.set_sync_params({
