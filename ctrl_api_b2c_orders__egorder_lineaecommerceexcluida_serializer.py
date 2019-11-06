9c1511aee1627f7a6cfb46a4f896bb976dba5a4a
Jesus Zornoza <jesus.yeboyebo@gmail.com>
2019-11-06 13:07:21 +0100

    Añadir campo viajecreado a false en eg_lineasecommerceexcluidas en sincro pedidos web

--- ctrl_api_b2c_orders__egorder_lineaecommerceexcluida_serializer.py
+++ ctrl_api_b2c_orders__egorder_lineaecommerceexcluida_serializer.py
@@ -6,11 +6,12 @@ from controllers.base.default.serializers.default_serializer import DefaultSeria
 class EgLineaEcommerceExcluida(DefaultSerializer):
 
     def get_data(self):
-        print("//////////////codALmacen serializador: " , self.init_data["almacen"])
+
         if not self.init_data["almacen"] or str(self.init_data["almacen"]) == "" or str(self.init_data["almacen"]) == "None" or self.init_data["almacen"] == None:
             return False
 
         self.set_string_value("codalmacen", str(self.init_data["almacen"]))
         self.set_string_relation("codcomanda", "codcomanda", max_characters=12)
+        self.set_string_value("viajecreado", False)
 
         return True
