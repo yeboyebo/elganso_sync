496803b0066abe95d118f94651618ab226c58ea6
Jesus Zornoza <jesus.yeboyebo@gmail.com>
2020-11-03 12:28:57 +0100

    Devoluciones Mirakl New

---
+++ ctrl_api_mirakl_returnsnew__eg_returnsnew_download.py
@@ -0,0 +1,187 @@
+from YBLEGACY import qsatype
+import xmltodict
+import json
+import xml.etree.cElementTree as ET
+from xml.etree.ElementTree import tostring
+from datetime import datetime, timedelta
+
+from controllers.base.mirakl.returns.controllers.returns_download import ReturnsDownload
+from controllers.api.mirakl.returnsnew.serializers.returnnew_serializer import ReturnNewSerializer
+from models.flfact_tpv.objects.egreturn_raw import EgReturn
+from controllers.base.mirakl.returns.serializers.ew_devolucioneseciweb_serializer import DevolucioneseciwebSerializer
+from models.flfact_tpv.objects.ew_devolucioneseciweb_raw import EwDevolucioneseciweb
+
+
+class EgMiraklReturnsNewDownload(ReturnsDownload):
+
+    fecha_sincro = ""
+    esquema = "DEVOLSNEW_ECI_WEB"
+    codtienda = "AEVV"
+
+    def __init__(self, params=None):
+        super().__init__("egmiraklreturnsnew", params)
+
+        returns_params = self.get_param_sincro('miraklReturnsNewDownload')
+        self.returns_url = returns_params['url']
+        self.returns_test_url = returns_params['test_url']
+
+        self.set_sync_params(self.get_param_sincro('mirakl'))
+
+    def get_data(self):
+        returns_url = self.returns_url if self.driver.in_production else self.returns_test_url
+        fecha = self.dame_fechasincrotienda(self.esquema, self.codtienda)
+        if fecha and fecha != "None" and fecha != "":
+            self.fecha_sincro = fecha
+        else:
+            self.fecha_sincro = "2020-11-03T00:00:01Z"
+
+
+        # Tmp. Para pruebas. Quitar en producción
+        #self.fecha_sincro = "2000-01-01T00:00:01Z"
+        result = self.send_request("get", url=returns_url.format(self.fecha_sincro))
+        #result = json.loads(json.dumps({"messages": [{"body": "{'message':{'return_info':'Le informamos que se ha iniciado un proceso de devolución con abono al cliente mediante TPV en Centro Comercial.','type':'R10', 'collection_address':'Calle Cerrajeros 31. Parque Logístico Módulo 4. 28830. San Fernando de Henares. Madrid Calle Cerrajeros 31. Parque Logístico Módulo 4. 28830. San Fernando de Henares. Madrid', 'returns': [{'ean':'008433614139774', 'order_line_id':'20190813153200-UATG111970173-A-1','quantity':'1'},{'ean':'008433614147113', 'order_line_id':'20190813153200-UATG111970173-A-1','quantity':'1'}]}}","commercial_id": "00100900293869620200616112759_1","date_created": "2020-06-17T10:35:33Z","from_id": "1","from_name": "Operator","from_type": "OPERATOR","id": "1006718","order_id": "20190813153200-UATG111970173-A","read": "False","subject": "R10 - Return done in store","to_shop_archived": "False","to_shop_id": "2076","to_shop_name":"Mkp Sterling Directa Cloud008 Prov 01"},{"body": "{'message':{'return_info':'Le informamos que se ha iniciado un proceso de devolución con abono al cliente mediante TPV en Centro Comercial.','type':'R02', 'collection_address':'Calla Falsa Numero 1 Almansa', 'returns': [{'ean':'008445005237662', 'order_line_id':'20200305203757-UATG251093894-A-1','quantity':'1'}]}}","commercial_id": "00100900293869620200616112759_1","date_created": "2020-06-17T10:35:33Z","from_id": "1","from_name": "Operator","from_type": "OPERATOR","id": "1006718","order_id": "20200305203757-UATG251093894-A","read": "False","subject": "R02 - Return done in store","to_shop_archived": "False","to_shop_id": "2076","to_shop_name":"Mkp Sterling Directa Cloud008 Prov 01"}]}))
+        return result
+
+    def process_all_data(self, all_data):
+        if "messages" not in all_data:
+            self.log("Éxito", "No hay datos que sincronizar")
+            return False
+
+        processData = False
+        for data in all_data["messages"]:
+            try:
+                if "body" not in data:
+                    continue
+
+                bodyMensaje = str(data["body"])
+                bodyMensaje = bodyMensaje.replace("\"", "__aqcomillas__")
+                bodyMensaje = bodyMensaje.replace("'", "\"")
+                bodyMensaje = bodyMensaje.replace("__aqcomillas__", "'")
+                mensajeDevol = json.loads(bodyMensaje)
+                fecha = data["date_created"]
+                if self.fecha_sincro != "":
+                    if fecha > self.fecha_sincro:
+                        self.fecha_sincro = fecha
+                else:
+                    self.fecha_sincro = fecha
+                tipoMsg = str(mensajeDevol['message']['type'])
+                if tipoMsg != "R10" and tipoMsg != "R02":
+                    continue
+                processData = True
+                if self.process_data(data):
+                    self.success_data.append(data)
+            except Exception as e:
+                self.sync_error(data, e)
+
+        if not self.guarda_fechasincrotienda(self.esquema, self.codtienda):
+            self.log("Error", "Falló al guardar fecha última sincro")
+            return self.small_sleep
+
+        if processData == False:
+            self.log("Éxito", "No hay datos que sincronizar")
+            return False
+
+        return True
+
+    def process_data(self, data):
+        if not data:
+            self.error_data.append(data)
+            return False
+
+        idComandaO = qsatype.FLUtil.quickSqlSelect("ew_ventaseciweb", "idtpv_comanda", "idweb = '{}'".format(data["order_id"]))
+
+        bodyMensaje = str(data["body"])
+        bodyMensaje = bodyMensaje.replace("\"", "__aqcomillas__")
+        bodyMensaje = bodyMensaje.replace("'", "\"")
+        bodyMensaje = bodyMensaje.replace("__aqcomillas__", "'")
+        mensajeDevol = json.loads(bodyMensaje)
+        for devolucion in mensajeDevol['message']['returns']:
+            barcode = str(devolucion["ean"])[2:15]
+            cantidad = int(devolucion["quantity"])
+            cantDev = int(qsatype.FLUtil.quickSqlSelect("tpv_lineascomanda", "cantdevuelta", "idtpv_comanda = {} AND barcode = '{}'".format(idComandaO, barcode)))
+            if not cantDev or cantDev == "None":
+                cantDev = 0
+
+            if cantDev >= cantidad:
+                self.log("Error", "La línea con barcode {} de la venta {} ya ha sido procesada".format(barcode, data["order_id"]))
+                return False
+
+        data["valdemoro"] = False
+        eciweb_data = DevolucioneseciwebSerializer().serialize(data)
+        if not eciweb_data:
+            self.error_data.append(data)
+            return False
+
+        idComanda = self.masAccionesProcessData(eciweb_data)
+        if not idComanda or idComanda == "None":
+            raise NameError("No se pudo crear la devolución")
+
+        eciweb_data["idtpv_comanda"] = idComanda
+        eciweb_data["datosdevol"] = data["body"]
+
+        devoleciweb = EwDevolucioneseciweb(eciweb_data)
+
+        devoleciweb.save()
+
+        return True
+
+    def masAccionesProcessData(self, eciweb_data):
+        eciweb_data["datosdevol"] = json.loads(json.dumps(eciweb_data["datosdevol"]))
+        return_data = self.get_return_serializer().serialize(eciweb_data)
+        if not return_data:
+            return False
+
+        return_data["valdemoro"] = False
+        objReturn = EgReturn(return_data)
+        objReturn.save()
+        idtpvDevol = objReturn.cursor.valueBuffer("idtpv_comanda")
+        codComandaDevol = qsatype.FLUtil.quickSqlSelect("tpv_comandas", "codcomandaDevol", "idtpv_comanda = '{}'".format(idtpvDevol))
+        idtpvVenta = qsatype.FLUtil.quickSqlSelect("tpv_comandas", "idtpv_comanda", "codigo = '{}'".format(codComandaDevol))
+
+        qL = qsatype.FLSqlQuery()
+        qL.setSelect("barcode, cantidad")
+        qL.setFrom("tpv_lineascomanda")
+        qL.setWhere("idtpv_comanda = {}".format(idtpvDevol))
+
+        if not qL.exec_():
+            syncppal.iface.log("Error. Falló la query al obtener los datos de devolución {}".format(idtpvDevol), "egmiraklreturns")
+            return False
+
+        while qL.first():
+            cantDev = qsatype.FLUtil.sqlSelect("tpv_lineascomanda", "cantdevuelta", "idtpv_comanda = {} AND barcode = '{}'".format(idtpvVenta, qL.value("barcode"))) + (int(qL.value("cantidad"))*-1)
+            if not qsatype.FLUtil.sqlUpdate("tpv_lineascomanda", ["cantdevuelta"], [cantDev], "idtpv_comanda = {} AND barcode = '{}'".format(idtpvVenta, qL.value("barcode"))):
+                return False
+
+        return objReturn.cursor.valueBuffer("idtpv_comanda")
+
+    def get_return_serializer(self):
+        return ReturnNewSerializer()
+
+    def after_sync(self):
+        if not self.guarda_fechasincrotienda(self.esquema, self.codtienda):
+            self.log("Error", "Falló al guardar fecha última sincro")
+            return self.small_sleep
+
+        if self.success_data:
+            self.log("Éxito", "Las siguientes devoluciones se han sincronizado correctamente: {}".format([order["order_id"] for order in self.success_data]))
+
+        return self.large_sleep
+
+    def guarda_fechasincrotienda(self, esquema, codtienda):
+        fecha = str(self.fecha_sincro)[:10]
+
+        fechaSeg = datetime.strptime(self.fecha_sincro, '%Y-%m-%dT%H:%M:%SZ')
+        fecha1Seg = fechaSeg + timedelta(seconds=1)
+        hora = str(fecha1Seg)[11:19]
+
+        idsincro = qsatype.FLUtil.sqlSelect("tpv_fechasincrotienda", "id", "esquema = '{}' AND codtienda = '{}'".format(esquema, codtienda))
+
+        if idsincro:
+            qsatype.FLSqlQuery().execSql("UPDATE tpv_fechasincrotienda SET fechasincro = '{}', horasincro = '{}' WHERE id = {}".format(fecha, hora, idsincro))
+        else:
+            qsatype.FLSqlQuery().execSql("INSERT INTO tpv_fechasincrotienda (codtienda, esquema, fechasincro, horasincro) VALUES ('{}', '{}', '{}', '{}')".format(codtienda, esquema, fecha, hora))
+
+        return True
+
+    def dame_fechasincrotienda(self, esquema, codtienda):
+        return qsatype.FLUtil.sqlSelect("tpv_fechasincrotienda", "fechasincro || 'T' || horasincro || 'Z'", "esquema = '{}' AND codtienda = '{}'".format(esquema, codtienda))
---
+++ ctrl_api_mirakl_returnsnew__returnnew_serializer.py
@@ -0,0 +1,171 @@
+from YBLEGACY import qsatype
+from YBLEGACY.constantes import *
+
+from datetime import datetime
+import time
+import json
+from models.flsyncppal import flsyncppal_def as syncppal
+from controllers.base.default.serializers.default_serializer import DefaultSerializer
+from controllers.api.mirakl.returnsnew.serializers.return_line_serializer import ReturnLineSerializer
+from controllers.api.mirakl.returns.serializers.idl_ecommercedevoluciones_serializer import IdlEcommerceDevolucionesSerializer
+from controllers.base.mirakl.orders.serializers.order_payment_serializer import OrderPaymentSerializer
+from controllers.base.mirakl.orders.serializers.cashcount_serializer import CashCountSerializer
+
+class ReturnNewSerializer(DefaultSerializer):
+
+    def get_data(self):
+        qC = qsatype.FLSqlQuery()
+        qC.setSelect("c.*")
+        qC.setFrom("tpv_comandas c INNER JOIN ew_ventaseciweb v on c.idtpv_comanda = v.idtpv_comanda")
+        qC.setWhere("v.idweb = '{}'".format(self.init_data["idventaweb"]))
+        if not qC.exec_():
+            syncppal.iface.log("Error. Falló la query al obtener los datos de la venta original para {}".format(self.init_data["idventaweb"]), "egmiraklreturns")
+            return False
+
+        if not qC.first():
+            syncppal.iface.log("Error. No se encontró la venta original para {}".format(self.init_data["idventaweb"]), "egmiraklreturns")
+            return False
+
+        codigo = self.get_codigo()
+        self.set_string_value("codigo", codigo, max_characters=15)
+
+        codtienda = self.get_codtienda()
+        codalmacen = qsatype.FLUtil.quickSqlSelect("tpv_tiendas", "codalmacen", "codtienda = '{}'".format(codtienda))
+        punto_venta = qsatype.FLUtil.sqlSelect("tpv_puntosventa", "codtpv_puntoventa", "codtienda = '{}'".format(codtienda))
+
+        self.set_string_value("codtpv_puntoventa", punto_venta)
+        self.set_string_value("codalmacen", codalmacen)
+        self.set_string_value("codtienda", codtienda)
+        self.set_string_value("codcomandadevol", str(qC.value("c.codigo")))
+        self.set_string_value("tipodoc", "DEVOLUCION")
+        self.set_string_value("codtpv_agente", "0350")
+        self.set_data_value("editable", True)
+        self.set_data_value("tasaconv", 1)
+        self.set_data_value("ptesincrofactura", False)
+
+        utcCreatedDtate = datetime.strptime(self.get_init_value("date_created"), '%Y-%m-%dT%H:%M:%SZ')
+        localCreatedDate = self.utcToLocal(utcCreatedDtate)
+        fecha = str(localCreatedDate)[:10]
+        hora = str(localCreatedDate)[-8:]
+        self.set_string_value("fecha", fecha)
+        self.set_string_value("hora", hora)
+        self.set_string_value("codpostal", "-")
+        self.set_string_value("ciudad", "-")
+        self.set_string_value("provincia", "-")
+        self.set_string_value("codpais", str(qC.value("c.codpais")))
+        self.set_string_value("telefono1", "-")
+        self.set_string_value("nombrecliente", str(qC.value("c.nombrecliente")))
+
+        bodyMensaje = str(self.get_init_value("datosdevol"))
+        bodyMensaje = bodyMensaje.replace("\"", "__aqcomillas__")
+        bodyMensaje = bodyMensaje.replace("'", "\"")
+        bodyMensaje = bodyMensaje.replace("__aqcomillas__", "'")
+        mensajeDevol = json.loads(bodyMensaje)
+
+        self.set_string_value("direccion", str(mensajeDevol["message"]["collection_address"]), max_characters=100)
+        self.set_string_value("codserie", self.get_codserie())
+        codejercicio = fecha = str(localCreatedDate)[:4]
+        self.set_string_value("codejercicio", codejercicio)
+        self.set_string_value("codpago", self.get_codpago(), max_characters=10)
+        self.set_string_value("egcodfactura", "NULL")
+        if "lines" not in self.data["children"]:
+            self.data["children"]["lines"] = []
+
+        total = 0
+        neto = 0
+        for item in mensajeDevol["message"]["returns"]:
+            if "ean" not in item:
+                return False
+            item.update({
+                "codalmacen": codalmacen,
+                "codcomanda": codigo,
+                "idcomandaO": str(qC.value("c.idtpv_comanda"))
+            })
+
+            line_data = ReturnLineSerializer().serialize(item)
+            total = total + line_data["pvptotaliva"]
+            neto = neto + line_data["pvptotal"]
+            self.data["children"]["lines"].append(line_data)
+
+        new_init_data = self.init_data.copy()
+        new_init_data.update({
+            "codcomanda": codigo,
+            "fecha": self.data["fecha"]
+        })
+
+        self.set_data_value("total", total)
+        self.set_data_value("neto", neto)
+        self.set_data_value("totaliva", total - neto)
+        self.set_string_value("estado", "Abierta")
+        self.set_data_value("pagado", 0)
+        self.set_data_value("pendiente", total)
+
+        idl_ecommerceDev = IdlEcommerceDevolucionesSerializer().serialize(new_init_data)
+        self.data["children"]["idl_ecommercedevoluciones"] = idl_ecommerceDev
+
+        return True
+
+    def get_fecha(self):
+        fecha = str(self.init_data["date_created"])[:10]
+        return fecha
+
+    def get_codtienda(self):
+        return "AEVV"
+
+    def get_codserie(self):
+        pais = self.data["codpais"]
+        codpostal = self.data["codpostal"]
+
+        codpais = None
+        codserie = "A"
+        codpostal2 = None
+
+        if not pais or pais == "":
+            return codserie
+
+        codpais = qsatype.FLUtil.quickSqlSelect("paises", "codpais", "UPPER(codpais) = '{}'".format(pais.upper()))
+        if not codpais or codpais == "":
+            return codserie
+
+        if codpais != "ES":
+            codserie = "X"
+        elif codpostal and codpostal != "":
+            codpostal2 = codpostal[:2]
+            if codpostal2 == "35" or codpostal2 == "38" or codpostal2 == "51" or codpostal2 == "52":
+                codserie = "X"
+
+        return codserie
+
+    def get_codejercicio(self):
+        date = self.init_data["date_created"][:10]
+        splitted_date = date.split("-")
+
+        return splitted_date[0]
+
+    def get_hora(self):
+        hour = self.init_data["date_created"][-9:-1]
+        hour = "23:59:59" if hour == "00:00:00" else hour
+        return hour
+
+    def get_codpago(self):
+        return "TARJ"
+
+    def get_codigo(self):
+        prefix = "EDV"
+        ultima_vta = None
+
+        id_ultima = qsatype.FLUtil.sqlSelect("tpv_comandas", "codigo", "codigo LIKE '{}%' ORDER BY codigo DESC LIMIT 1".format(prefix))
+
+        if id_ultima:
+            ultima_vta = parseInt(str(id_ultima)[-(12 - len(prefix)):])
+        else:
+            ultima_vta = 0
+
+        ultima_vta = ultima_vta + 1
+
+        return "{}{}".format(prefix, qsatype.FactoriaModulos.get("flfactppal").iface.cerosIzquierda(str(ultima_vta), 12 - len(prefix)))
+
+    def utcToLocal(self, utc_datetime):
+        now_timestamp = time.time()
+        offset = datetime.fromtimestamp(now_timestamp) - datetime.utcfromtimestamp(now_timestamp)
+        return utc_datetime + offset
