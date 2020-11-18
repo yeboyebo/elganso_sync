8248ba4d728298f60888420677bc5d6ae6a3c975
xavikortes <javiercortesveliz@gmail.com>
2020-11-18 18:11:49 +0100

    Planes de precios

--- ctrl_api_amazon_prices__price_serializer.py
+++ ctrl_api_amazon_prices__price_serializer.py
@@ -9,7 +9,9 @@ class PriceSerializer(DefaultSerializer):
 
         self.set_string_relation("Price//SKU", "aa.barcode")
 
-        tarifa = float(self.init_data["at.pvp"])
+        tarifa = None
+        if self.init_data["at.pvp"]:
+            tarifa = float(self.init_data["at.pvp"])
 
         if not tarifa or tarifa == 0 or tarifa == 0.0:
             tarifa = float(self.init_data["a.pvp"])
@@ -17,9 +19,11 @@ class PriceSerializer(DefaultSerializer):
         self.set_data_value("Price//StandardPrice//xml_node_value", tarifa)
         self.set_string_value("Price//StandardPrice//@currency", "EUR")
 
-        # Aqui va la oferta
-        # self.set_string_relation("Price//Sale//StartDate", fechaInicio)
-        # self.set_string_relation("Price//Sale//EndDate", fechaFin)
-        # self.set_data_relation("Price//Sale//SalePrice", precioOferta)
+        if self.init_data["pp.desde"] and self.init_data["pp.hasta"] and self.init_data["app.pvp"] and self.init_data["pp.horadesde"] and self.init_data["pp.horahasta"]:
+            self.set_string_value("Price//Sale//StartDate", "{}T{}".format(self.init_data["pp.desde"], self.init_data["pp.horadesde"]))
+            self.set_string_value("Price//Sale//EndDate", "{}T{}".format(self.init_data["pp.hasta"], self.init_data["pp.horahasta"]))
+
+            self.set_data_relation("Price//Sale//SalePrice//xml_node_value", "app.pvp")
+            self.set_string_value("Price//Sale//SalePrice//@currency", "EUR")
 
         return True
