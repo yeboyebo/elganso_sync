c0ecaf164f1856441cd7871b4870f1d8843a008e
Jesus Zornoza <jesus.yeboyebo@gmail.com>
2021-08-19 11:43:55 +0200

    Cambio sincro inventory. Añadir cantreservada

--- ctrl_api_magento2_inventory__inventory_serializer.py
+++ ctrl_api_magento2_inventory__inventory_serializer.py
@@ -1,3 +1,5 @@
+from YBLEGACY import qsatype
+from YBLEGACY.constantes import *
 from controllers.base.default.serializers.default_serializer import DefaultSerializer
 
 
@@ -15,10 +17,51 @@ class InventorySerializer(DefaultSerializer):
         disponible = self.init_data["s.disponible"]
         if self.init_data["s.disponible"] > 0:
             status = 1
-        # else:
-            # status = 0
-            # disponible = 0
 
-        self.set_string_value("quantity", disponible)
+        hoy = qsatype.Date()
+        stockReservado = qsatype.FLUtil.sqlSelect("eg_anulacionstockreservado", "idstock", "idstock = {} AND activo = true AND fechatope >= '{}'".format(self.init_data["s.idstock"], hoy))
+        if stockReservado and stockReservado != 0:
+            cantA = parseInt(qsatype.FLUtil.sqlSelect("eg_anulacionstockreservado", "cantstockreservadoanulado", "idstock = {} AND activo = true AND fechatope >= '{}'".format(self.init_data["s.idstock"], hoy)))
+            if not cantA:
+                cantA = 0
+
+            qty = parseInt(self.dame_stock(self.init_data["s.disponible"])) + cantA
+        else:
+            qty = parseInt(self.dame_stock(self.init_data["s.disponible"]))
+
+        aListaAlmacenes = self.dame_almacenessincroweb().split(",")
+        if str(self.init_data["s.codalmacen"]) not in aListaAlmacenes:
+            raise NameError("Error. Existe un registro cuyo almacén no está en la lista de almacenes de sincronización con Magento. " + str(self.init_data["ssw.idssw"]))
+
+        cant_disponible = qty
+        if str(str(self.init_data["s.codalmacen"])) != "AWEB":
+            cant_reservada = self.get_cantreservada(str(self.init_data["s.codalmacen"]))
+            cant_disponible = parseFloat(qty) - parseFloat(cant_reservada)
+            if cant_disponible < 0:
+                cant_disponible = 0
+
+        self.set_string_value("quantity", cant_disponible)
         self.set_string_value("status", status)
         return True
+
+    def dame_stock(self, disponible):
+        if not disponible or isNaN(disponible) or disponible < 0:
+            return 0
+
+        return disponible
+
+    def dame_almacenessincroweb(self):
+
+        listaAlmacenes = qsatype.FLUtil.sqlSelect("param_parametros", "valor", "nombre = 'ALMACENES_SINCRO'")
+        if not listaAlmacenes or listaAlmacenes == "" or str(listaAlmacenes) == "None" or listaAlmacenes == None:
+            return "AWEB"
+
+        return listaAlmacenes
+
+    def get_cantreservada(self, codalmacen):
+
+        cant_reservada = qsatype.FLUtil.sqlSelect("param_parametros", "valor", "nombre = 'RSTOCK_" + str(codalmacen) + "'")
+        if not cant_reservada or cant_reservada == "" or str(cant_reservada) == "None" or cant_reservada == None:
+            return 0
+
+        return parseFloat(cant_reservada)
--- ctrl_api_magento2_inventory__mg2_inventory_upload.py
+++ ctrl_api_magento2_inventory__mg2_inventory_upload.py
@@ -51,7 +51,7 @@ class Mg2InventoryUpload(InventoryUpload):
         body = []
 
         q = qsatype.FLSqlQuery()
-        q.setSelect("ssw.idssw, aa.referencia, aa.talla, s.disponible, s.codalmacen")
+        q.setSelect("ssw.idssw, aa.referencia, aa.talla, s.disponible, s.codalmacen, s.idstock")
         q.setFrom("articulos a INNER JOIN atributosarticulos aa ON a.referencia = aa.referencia INNER JOIN stocks s ON aa.barcode = s.barcode LEFT OUTER JOIN eg_sincrostockwebinmediato ssw ON s.idstock = ssw.idstock")
         q.setWhere("NOT ssw.sincronizado OR ssw.sincronizado = false ORDER BY aa.referencia LIMIT 25")
 
