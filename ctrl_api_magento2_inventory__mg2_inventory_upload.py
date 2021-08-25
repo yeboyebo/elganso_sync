9aa0ff67002413d0ff01e64329713844325ac228
Jesus Zornoza <jesus.yeboyebo@gmail.com>
2021-08-25 12:08:24 +0200

    Cambio aftersync inventory

--- ctrl_api_magento2_inventory__mg2_inventory_upload.py
+++ ctrl_api_magento2_inventory__mg2_inventory_upload.py
@@ -78,6 +78,12 @@ class Mg2InventoryUpload(InventoryUpload):
     def after_sync(self, response_data=None):
         qsatype.FLSqlQuery().execSql("UPDATE eg_sincrostockwebinmediato SET sincronizado = true WHERE idssw IN ({})".format(self._ssw))
 
+        qsatype.FLSqlQuery().execSql("DELETE FROM eg_sincromovistockweb WHERE sincronizado = false AND idstock IN (SELECT idstock from eg_sincrostockwebinmediato where idssw IN ({}))".format(self._ssw))
+
+        qsatype.FLSqlQuery().execSql("INSERT INTO eg_sincromovistockweb (idstock, idmovimiento, referencia, talla, barcode, sincronizado, cantidad, fecha, hora) (SELECT m.idstock, m.idmovimiento, l.referencia, l.talla, l.barcode, false, lv.cantenviada, CURRENT_DATE, CURRENT_TIME FROM tpv_comandas c INNER JOIN tpv_lineascomanda l ON c.idtpv_comanda = l.idtpv_comanda INNER JOIN eg_lineasecommerceexcluidas e ON l.idtpv_linea = e.idtpv_linea INNER JOIN tpv_lineasmultitransstock lv ON (e.idviajemultitrans = lv.idviajemultitrans AND l.barcode = lv.barcode) INNER JOIN movistock m ON lv.idlinea = m.idlineatto INNER JOIN stocks s ON (s.idstock = m.idstock AND s.codalmacen = e.codalmacen) INNER JOIN eg_sincrostockwebinmediato i ON s.idstock = i.idstock WHERE c.fecha > CURRENT_DATE-30 AND e.pedidoanulado = false AND e.pedidoenviado = false AND e.pedidopreparado = false AND e.faltantecreada = false AND i.idssw IN ({}))".format(self._ssw))
+
+        qsatype.FLSqlQuery().execSql("INSERT INTO eg_sincromovistockweb (idstock, idmovimiento, referencia, talla, barcode, sincronizado, cantidad, fecha, hora) (SELECT m.idstock, m.idmovimiento, l.referencia, l.talla, l.barcode, false, l.cantidad, CURRENT_DATE, CURRENT_TIME from idl_ecommerce e INNER JOIN tpv_comandas c on e.idtpv_comanda = c.idtpv_comanda INNER JOIN tpv_lineascomanda l on c.idtpv_comanda = l.idtpv_comanda INNER JOIN movistock m on l.idtpv_linea = m.idlineaco INNER JOIN stocks s on m.idstock = s.idstock INNER JOIN eg_sincrostockwebinmediato i ON s.idstock = i.idstock left outer join eg_lineasecommerceexcluidas ex on l.idtpv_linea = ex.idtpv_linea WHERE e.confirmacionenvio = 'No' AND idlogenvio > 0 AND idlogenvio <> 999999 AND c.codtienda = 'AWEB' AND (c.codigo like 'WEB%' or c.codigo like 'WDV') AND c.fecha >= CURRENT_DATE-30 AND m.estado = 'PTE' AND ex.id is null AND s.codalmacen = 'AWEB' AND i.idssw IN ({}))".format(self._ssw))
+
         self.log("Exito", "Stock sincronizado correctamente")
 
         return self.small_sleep
