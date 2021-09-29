5175febc50f83e319b4bf7a07477211f20ac7abc
Jesus Zornoza <jesus.yeboyebo@gmail.com>
2021-09-29 09:20:13 +0200

    Cambio planes precios MG2

--- ctrl_api_magento2_special_price__mg2_special_price_upload.py
+++ ctrl_api_magento2_special_price__mg2_special_price_upload.py
@@ -80,7 +80,7 @@ class Mg2SpecialPriceUpload(TierpriceUpload):
         # q.setSelect("ls.id, at.referencia, at.talla, ap.pvp, p.desde || ' ' || p.horadesde, p.hasta || ' ' || p.horahasta, mg.idmagento")
         q.setSelect("ls.id, at.referencia, at.talla, ap.pvp, p.desde || ' ' || p.horadesde, ap.activo, p.hasta || ' ' || p.horahasta, mg.idmagento")
         q.setFrom("eg_planprecios p INNER JOIN eg_articulosplan ap ON p.codplan = ap.codplan INNER JOIN atributosarticulos at ON ap.referencia = at.referencia INNER JOIN eg_tiendasplanprecios tp ON p.codplan = tp.codplan INNER JOIN mg_storeviews mg ON tp.codtienda = mg.egcodtiendarebajas INNER JOIN lineassincro_catalogo ls ON (p.codplan = ls.idobjeto AND at.referencia || '-' || at.talla || '-' || mg.idmagento = ls.descripcion)")
-        q.setWhere("p.elgansociety = FALSE AND ls.id IN (SELECT id FROM lineassincro_catalogo WHERE sincronizado = FALSE AND tiposincro = 'Planificador Precios' ORDER BY id LIMIT 20) GROUP BY ls.id,at.referencia, at.talla, ap.pvp, p.desde || ' ' || p.horadesde, p.hasta || ' ' || p.horahasta, mg.idmagento, ap.activo ORDER BY ls.id")        
+        q.setWhere("p.elgansociety = FALSE AND ls.id IN (SELECT id FROM lineassincro_catalogo WHERE sincronizado = FALSE AND tiposincro = 'Planificador Precios' ORDER BY id LIMIT 20) GROUP BY ls.id,at.referencia, at.talla, ap.pvp, p.desde || ' ' || p.horadesde, p.hasta || ' ' || p.horahasta, mg.idmagento, ap.activo ORDER BY ls.id")
 
         q.exec_()
 
